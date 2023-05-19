40da08a38228e60db391ba1bea62c988e7045b61
nowJav <93018741+nowJav@users.noreply.github.com>
2023-05-19 00:26:30 -0600

    Add files via upload

--- Lab8.ino
+++ Lab8.ino
@@ -1,15 +1,49 @@
-int rele=8;
+// encendido y apagado de led con muestra en el monitor serial
+/*int rele = 8;
 
 void setup() {
-pinMode(8, OUTPUT);
+  pinMode(rele, OUTPUT);
+  Serial.begin(9600);
 }
-
 
 void loop() {
-digitalWrite(rele, HIGH);
-delay(2000); 
-digitalWrite(rele, LOW); 
-delay(5000); 
+  digitalWrite(rele, HIGH);
+  Serial.println("Relé apagado");
+  delay(2000); 
+  
+  digitalWrite(rele, LOW);
+  Serial.println("Relé encendido");
+  delay(5000); 
+}*/
+// encendido y apagado controlado desde el monitor serial
+int rele = 8;
 
-
+void setup() {
+  pinMode(rele, OUTPUT);
+  Serial.begin(9600);
 }
+
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    executeCommand(command);
+  }
+}
+
+void executeCommand(char command) {
+  switch (command) {
+    case '1':
+      digitalWrite(rele, HIGH);
+      Serial.println("Relé apagado");
+      break;
+    case '0':
+      digitalWrite(rele, LOW);
+      Serial.println("Relé encendido");
+      break;
+    default:
+      Serial.println("Comando no reconocido");
+      break;
+  }
+}
+
+
